b710cf7b45b0ff27f1180772edf6bd2e4b62c09f
ArvinthK <105003598+ArvinthKumareson@users.noreply.github.com>
2024-11-26 16:17:46 +0800

    Initial server.js

---
+++ server.js
@@ -0,0 +1,36 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+
+// Create Express app
+const app = express();
+
+// Enable CORS for all origins
+app.use(cors());
+
+// MongoDB connection URI
+const MONGO_URI = 'mongodb+srv://damaine334:Angulimala123@cluster0.vc4zr.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0';
+
+mongoose.connect(MONGO_URI, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  serverSelectionTimeoutMS: 30000
+}).then(() => {
+  console.log('Connected to MongoDB');
+}).catch((err) => {
+  console.log('Error connecting to MongoDB:', err);
+});
+
+// Define Mongoose schema and model for users
+const userSchema = new mongoose.Schema({
+  name: String,
+  icNumber: String,
+  pic1: String, // Base64 string for Pic1
+  pic2: String, // Base64 string for Pic2
+  status: { type: String, enum: ['verified', 'notVerified'], default: 'notVerified' }, // Verified status
+  createdAt: {
+    type: Date,
+    default: Date.now
+  }
+});
